21903ce6b5176bb857ad2136a761b6243a366d07
SimenHus <simen.spam@hotmail.com>
2023-05-31 22:22:56 +0200

    Applied multiprocess for plotting (unfinished)

--- Modules/LiveGrapher.py
+++ Modules/LiveGrapher.py
@@ -1,6 +1,10 @@
 import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+from random import randint
+from multiprocessing import Process
+from time import sleep, time
 
-class Grapher:
+class Grapher(Process):
 
     def __init__(self, dt: float):
         super().__init__()
@@ -12,8 +16,6 @@ class Grapher:
         self.data = {}
         self.fig = plt.figure()
         self.subplots = []
-        plt.ion()
-        plt.show()
 
     def addSubplot(self, dataNames: list):
         index = int(f'22{len(self.subplots)+1}')
@@ -31,19 +33,14 @@ class Grapher:
 
         self.subplots.append(ax)
 
-    def update(self, data: dict):
-        
-        size = len(data[list(data)[0]])
-        
-        for _ in range(size): self.t.append(self.t[-1]+self.dt)
-        self.t = self.t[size:]
+    def run(self):
+        ani = animation.FuncAnimation(self.fig,
+                                      self.anim,
+                                      interval=1,
+                                      blit=True)
+        plt.show()
 
-        for elem, value in data.items():
-            self.data[elem] = self.data[elem] + value
-            self.data[elem] = self.data[elem][size:]
-
-
-
+    def anim(self):
         for ax in self.subplots:
             ylim = ax.get_ylim()
             ax.set_xlim(self.t[0], self.t[-1])
@@ -56,31 +53,43 @@ class Grapher:
                 
                 line.set_data(self.t, self.data[line.get_label()])
 
+    def addData(self, data: dict):
+        
+        size = len(data[list(data)[0]])
+        
+        for _ in range(size): self.t.append(self.t[-1]+self.dt)
+        self.t = self.t[size:]
 
-        self.fig.canvas.draw()
-        self.fig.canvas.flush_events()
+        for elem, value in data.items():
+            self.data[elem] = self.data[elem] + value
+            self.data[elem] = self.data[elem][size:]
+
 
 tings = ['s', 'k']
 tings2 = ['x', 'y', 'z']
 
 app = Grapher(1)
+
 app.addSubplot(tings)
 app.addSubplot(tings2)
 
-from time import sleep, time
 
+def main():
+    while True:
+        startTime = time()
+        sampleSize = randint(1, 10)
+        uptDict = {
+            's': [randint(-100, 100)]*sampleSize,
+            'k': [randint(-120, 80)]*sampleSize,
+            'x': [randint(-100, 600)]*sampleSize,
+            'y': [randint(-50, 130)]*sampleSize,
+            'z': [randint(-120, 120)]*sampleSize,
+        }
+        app.addData(uptDict)
+        elapsedTime = (time() - startTime)*1000
+        print(f'Elapsed time: {elapsedTime:0.2f}ms')
 
-for i in range(200):
-    startTime = time()
-    uptDict = {
-        's': [i],
-        'k': [i**2],
-        'x': [i/(i+1)],
-        'y': [i**(1/2)],
-        'z': [2*i],
-    }
-    app.update(uptDict)
-    elapsedTime = (time() - startTime)*1000
-    print(f'Elapsed time: {elapsedTime:0.2f}ms')
-
-sleep(1000)
+if __name__ == '__main__':
+    app.start()
+    main()
+    app.join()
