6dc2b10a856e133c56a73615694cbcadc1599bf4
SimenHus <84506051+SimenHus@users.noreply.github.com>
2023-10-13 14:38:29 +0200

    Delete RC-WorkBranch directory

--- RC-WorkBranch/Backups and Misc/checkDevicesMisc.py
+++
@@ -1,9 +0,0 @@
-import evdev
-
-devices = [evdev.InputDevice(path) for path in evdev.list_devices()]
-for device in devices:
-    print(f'\n-------------------{device.path, device.name, device.phys}--------------')
-    # print(device.capabilities())
-    for infoCategory, content in device.capabilities(verbose=True).items():
-        print(f'\n------------{infoCategory}-----------')
-        for info in content: print(info)
--- RC-WorkBranch/README.md
+++
@@ -1,2 +0,0 @@
-# RC
-Repository to structure and contain code to my RC car project
