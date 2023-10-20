fb790863c29f96f70b4f9af1854235cf7f547afe
SimenHus <simen_vh@live.no>
2023-10-20 13:00:36 +0200

    Improved Quaternion object

--- CustomObjs/Quaternion.py
+++ CustomObjs/Quaternion.py
@@ -1,47 +1,45 @@
 
 import numpy as np
 
+from dataclasses import dataclass
+
+
+@dataclass
 class Quaternion:
-    def __init__(self, q0=np.array([1, 0, 0, 0])):
-        if len(q0) == 3: q0 = self.fromEulerAngles(q0)
-        elif q0.dtype == 'int32': q0 = q0.astype('float64')
-        self.__q = self.normalize(q0)
+    """Class representing a quaternion.
 
-    def q(self): return self.__q
-    def w(self): return self.__q[0]
-    def x(self): return self.__q[1]
-    def y(self): return self.__q[2]
-    def z(self): return self.__q[3]
+    Args:
+        eta (float): Eta/w/real part
+        epsilon (ndarray[3]): Epsilon/xyz/imaginary part
+    """
+    
+    eta: float
+    eps: 'np.ndarray[3]'
 
-    def fromEulerAngles(self, euler):
-        # Using 3-2-1 euler sequence
-        r, p, y = euler
+    def __post_init__(self) -> None:
+        norm = np.sqrt(self.eta**2 + sum(self.eps**2))
+        if not np.allclose(norm, 1):
+            self.eta /= norm
+            self.eps /= norm
+        if self.eta < 0:
+            self.eta *= -1
+            self.eps *= -1
 
-        cr, sr = np.cos(r/2), np.sin(r/2)
-        cp, sp = np.cos(p/2), np.sin(p/2)
-        cy, sy = np.cos(y/2), np.sin(y/2)
+    def toEulerAngles(self) -> 'np.ndarray[3]':
+        """
+        Returns the quaternion represented in euler angles
+        """
+        qw, epsilon = self
+        qx, qy, qz = epsilon
+        # To 3-2-1 euler sequence (ZYX)
+        roll = np.arctan2(2*(qw*qx+qy*qz), 1-2*(qx**2+qy**2))
+        pitch = -np.pi/2 + 2*np.arctan2(1+2*(qw*qy-qx*qz), 1-2*(qw*qy-qx*qz))
+        yaw = np.arctan2(2*(qw*qz+qx*qy), 1-2*(qy**2+qz**2))
 
-        # Euler angles to quaternions
-        quat = np.array([
-            cr*cp*cy + sr*sp*sy,
-            sr*cp*cy - cr*sp*sy,
-            cr*sp*cy - sr*sp*cy,
-            cr*cp*sy - sr*sp*cy
-        ]).reshape((4,))
-        return quat
-
-    def toEulerAngles(self, order='ZYX'):
-        qw, qx, qy, qz = self.__q
-        if order=='ZYX':
-            # To 3-2-1 euler sequence
-            roll = np.arctan2(2*(qw*qx+qy*qz), 1-2*(qx**2+qy**2))
-            pitch = -np.pi/2 + 2*np.arctan2(1+2*(qw*qy-qx*qz), 1-2*(qw*qy-qx*qz))
-            yaw = np.arctan2(2*(qw*qz+qx*qy), 1-2*(qy**2+qz**2))
-        if order=='XYZ':
-            # 1-2-3 sequence
-            roll = np.arctan2(2*(qy*qz+qw*qx), qw**2-qx**2-qy**2+qz**2)
-            pitch = np.arcsin(-2*(qx*qz-qw*qy))
-            yaw = np.arctan2(2*(qx*qy+qw*qz), qw**2+qx**2-qy**2-qz**2)
+        # 1-2-3 sequence (XYZ)
+        roll = np.arctan2(2*(qy*qz+qw*qx), qw**2-qx**2-qy**2+qz**2)
+        pitch = np.arcsin(-2*(qx*qz-qw*qy))
+        yaw = np.arctan2(2*(qx*qy+qw*qz), qw**2+qx**2-qy**2-qz**2)
 
         return np.array([roll, pitch, yaw])
     
@@ -52,87 +50,137 @@ class Quaternion:
         yaw = np.arctan2(2*(qw*qz+qx*qy), 1-2*(qy**2+qz**2))
 
         return -np.array([roll, pitch, yaw])
+
+    
+    def toRotationMatrix(self) -> 'np.ndarray[3, 3]':
+        """
+        Returns quaternion rotation represented as rotation matrix
+        """
+        eta, eps = self
+        e1, e2, e3 = eps
+
+        R = np.array([
+            [eta**2 + e1**2 - e2**2 - e3**2, 2 *
+                (e1*e2 - eta*e3), 2*(e1*e3 + eta*e2)],
+            [2*(e1*e2 + eta*e3), eta**2 - e1**2 +
+                e2**2 - e3**2, 2*(e2*e3 - eta*e1)],
+            [2*(e1*e3 - eta*e2), 2*(e2*e3 + eta*e1), eta**2 - e1**2 - e2**2 + e3**2]
+        ])
+        return R
+    
+    def conjugate(self) -> 'Quaternion':
+        return Quaternion(self.eta, -self.eps)
+    
+    def diff(self, other: 'Quaternion') -> 'Quaternion':
+        """
+        Return difference between two quaternions
+        """
+        return self.conjugate().multiply(other)
+    
+    def multiply(self, other: 'Quaternion') -> 'Quaternion':
+        eta, eps = self
+
+        m1 = np.vstack((
+            np.hstack((eta, -eps.T)),
+            np.hstack((eps.reshape((3, 1)), eta*np.eye(3) + self.skew(eps)))
+        ))
+        m2 = np.hstack((other.eta, other.eps))
+        result = m1@m2
+        return Quaternion(result[0], result[1:])
+
+    def coordinateTransform(self, other) -> 'Quaternion':
+        eta, eps = self
+        U = np.vstack(
+            -eps.T,
+            eta*np.eye(3) + self.skew(eps)
+        )
+        result = U@other
+        return Quaternion(result[0], result[1:])
     
 
     # Alternative way of getting the quaternion, see https://ahrs.readthedocs.io/en/latest/filters/aqua.html
-    def qAcc(self, acc):
+    @staticmethod
+    def fromAccelerometer(acc: 'np.ndarray[3]') -> 'Quaternion':
+        """
+        Get quaternion representation from accelerometer
+        """
         ax, ay, az = acc
         if az >= 0: qAcc = np.array([np.sqrt((az+1)/2), -ay/np.sqrt(2*az+2), ax/np.sqrt(2*az+2), 0]) 
         else:       qAcc = np.array([-ay/np.sqrt(2-2*az), np.sqrt((1-az)/2), 0, ax/np.sqrt(2-2*az)])
-        qAcc = Quaternion(qAcc)
-        return qAcc
+        return Quaternion(qAcc[0], qAcc[1:])
     
-    def qMag(self, qAcc, mag: np.ndarray):
-        l = qAcc.toRotationMatrix().T@mag
+    @staticmethod
+    def fromMagnetometer(qAcc: 'Quaternion', mag: 'np.ndarray[3]') -> 'Quaternion':
+        """
+        Get quaternion representation from accelerometer quaternion and magnetometer
+        """
+        l = qAcc.R.T@mag
         lx, ly, lz = l
         gamma = lx**2 + ly**2
         if lx >= 0: qMag = np.array([np.sqrt(gamma+lx*np.sqrt(gamma))/np.sqrt(2*gamma), 0, 0, ly/(np.sqrt(2)*np.sqrt(gamma+lx*np.sqrt(gamma)))])
         else:       qMag = np.array([ly/(np.sqrt(2)*np.sqrt(gamma-lx*np.sqrt(gamma))), 0, 0, np.sqrt(gamma-lx*np.sqrt(gamma))/np.sqrt(2*gamma)])
-        qMag = Quaternion(qMag)
-        return qMag
-    
-    
-    def toRotationMatrix(self, q=None):
-        if q is None: q = self.__q
-        nu, e1, e2, e3 = q
+        return Quaternion(qMag[0], qMag[1:])
 
-        R = np.array([
-            [nu**2 + e1**2 - e2**2 - e3**2, 2 *
-                (e1*e2 - nu*e3), 2*(e1*e3 + nu*e2)],
-            [2*(e1*e2 + nu*e3), nu**2 - e1**2 +
-                e2**2 - e3**2, 2*(e2*e3 - nu*e1)],
-            [2*(e1*e3 - nu*e2), 2*(e2*e3 + nu*e1), nu**2 - e1**2 - e2**2 + e3**2]
-        ])
-        return R
+    @staticmethod
+    def fromEulerAngles(euler: 'np.ndarray[3]') -> 'Quaternion':
+        """
+        Returns a Quaternion object derived from euler angles
+        """
+        # Using 3-2-1 euler sequence
+        r, p, y = euler
+
+        cr, sr = np.cos(r/2), np.sin(r/2)
+        cp, sp = np.cos(p/2), np.sin(p/2)
+        cy, sy = np.cos(y/2), np.sin(y/2)
+
+        # Euler angles to quaternions
+        q = np.array([
+            cr*cp*cy + sr*sp*sy,
+            sr*cp*cy - cr*sp*sy,
+            cr*sp*cy - sr*sp*cy,
+            cr*cp*sy - sr*sp*cy
+        ]).reshape((4,))
+        return Quaternion(q[0], q[1:])
     
-    def skew(self, vector):
+
+    @staticmethod
+    def skew(vector):
         skewMatrix = np.array([
             [0, -vector[2], vector[1]],
             [vector[2], 0, -vector[0]],
             [-vector[1], vector[0], 0]
         ])
         return skewMatrix
-
-    def normalize(self, q=None):
-        if q is None:
-            self.__q /= np.linalg.norm(self.__q)
-            q = self.__q
-        else: q /= np.linalg.norm(q)
-        return q
     
-    def conjugate(self, q=None):
-        if q is None: q = self.__q
-        return np.hstack((q[0], -q[1:]))
-    
-    def quaternionMultiplication(self, q1, q2):
-        nu1 = q1[0]
-        eps1 = np.array(q1[1:])
-        nu2 = q2[0]
-        eps2 = np.array(q2[1:])
+    @property
+    def R(self) -> 'np.ndarray[3, 3]': return self.toRotationMatrix()
+    @property
+    def w(self): return self.eta
+    @property
+    def x(self) -> float: return self.eps[0]
+    @property
+    def y(self) -> float: return self.eps[1]
+    @property
+    def z(self) -> float: return self.eps[3]
 
-        m1 = np.vstack((
-            np.hstack((nu1, -eps1.T)),
-            np.hstack((eps1.reshape((3, 1)), nu1*np.eye(3) + self.skew(eps1)))
-        ))
-        m2 = np.hstack((nu2, eps2))
-        return m1@m2
 
-    def coordinateTransform(self, q, w):
-        nu = q[0]
-        eps = np.array(q[1:])
-        U = np.vstack(
-            -eps.T,
-            nu*self.I3x3 + self.skew(eps)
-        )
-        return U@w
+    def __repr__(self):
+        return f'\u03B7 = {self.eta}, \u03B5 = {self.eps}'
 
-    def __add__(self, q):
-        return self.__q + q
+    def __add__(self, other: 'Quaternion') -> 'Quaternion':
+        return Quaternion(self.eta + other.eta, self.eps + other.eps)
     
     def __iter__(self):
-        for x in self.__q: yield x
+        return iter([self.eta, self.eps])
 
-    def __mul__(self, obj):
-        if type(obj) == Quaternion: return Quaternion(self.quaternionMultiplication(self.__q, obj.q()))
-        if type(obj) == np.ndarray and len(obj) == 3: return self.coordinateTransform(self.__q, obj)
-        if type(obj) == float or int: return Quaternion(self.__q*obj)
+    def __mul__(self, other) -> 'Quaternion':
+        """
+        Perform operation depending on type of other
+        """
+        if type(other) == Quaternion: return self.multiply(other)
+        if type(other) == np.ndarray and len(other) == 3: return self.coordinateTransform(other)
+        if type(other) == float or int: return Quaternion(self.eta*other, self.eps*other)
+
+    def __matmul__(self, other) -> 'Quaternion':
+        return self.multiply(other)
+
--- test.py
+++ test.py
@@ -1,12 +1,9 @@
-import socket
 
-# baddr = 'dc:71:96:4d:1b:5f' 
-baddr = 'a4:75:b9:d1:97:29' # Association Endpoint address for S23
-channel = 4
-with socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM) as s:
-    s.connect((baddr,channel))
-    # s_sock = server_sock.accept()
-    # print ("Accepted connection from "+address)
+from CustomObjs.Quaternion import Quaternion
+import numpy as np
 
-    data = s.recv(1024)
-    print ("received [%s]" % data)
+q1 = Quaternion(1, np.array([0]*3))
+
+q2 = Quaternion(0.5, np.array([0.5]*3))
+
+print(q1@q1)
